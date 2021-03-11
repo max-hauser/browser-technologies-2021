905d8c02e8c149c8c7e89076bbfd5e38f93b99bc
Max Hauser <hauser.m97@gmail.com>
2021-03-11 12:48:01 +0100

    added opdracht 2 folder

--- Opdracht1/js/script.js
+++ Opdracht1/js/script.js
@@ -1,10 +1,15 @@
 const figures = document.querySelectorAll('figure');
-        window.addEventListener('load', ()=>{
-            figures.forEach(figure =>{ figure.classList.add('hidden');});
-            figures[0].classList.remove('hidden');
-        });
+window.addEventListener('load', ()=>{
+    figures.forEach(figure =>{ 
+        figure.classList.add('hidden')
+    ;});
+    figures[0].classList.remove('hidden');
+});
 
-        for (let index = 0; index < figures.length; index++) {
-            const figure = figures[index];
-            figure.addEventListener('click', () => { if (index <= 1) { figures[index + 1].classList.remove('hidden');} });
-        }
+for (let index = 0; index < figures.length; index++) {
+    const figure = figures[index];
+    figure.addEventListener('click', () => {
+        if (index <= 1) {
+            figures[index + 1].classList.remove('hidden');}
+    });
+}
