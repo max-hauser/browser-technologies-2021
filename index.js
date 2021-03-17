83d8f078c649cfe6c902930c7f61086fe77fa1bb
Max Hauser <hauser.m97@gmail.com>
2021-03-17 20:06:56 +0100

    can add to db

--- index.js
+++ index.js
@@ -6,20 +6,55 @@ const mongo = require('mongodb');
 const MongoClient = require('mongodb').MongoClient;
 require('dotenv').config()
 
-// Connection URI
-const uri = process.env.URI;
-// Create a new MongoClient
-const client = new MongoClient(uri);
-async function run() {
-  try {
-    // Connect the client to the server
-    await client.connect();
-    // Establish and verify connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Connected successfully to server");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
+let db;
+const db_key = process.env.URI;
+MongoClient.connect(db_key, {useNewUrlParser: true, useUnifiedTopology: true}, function(err, client){
+  if(err){
+    throw err
+  }else{
+
   }
-}
-run().catch(console.dir);
+  db = client.db('mydb');
+});
+
+
+app
+
+.set('view engine', 'ejs')
+.use(express.static('static'))
+.use(express.json())
+.use(express.urlencoded())
+
+.get('/', function(req, res){
+  res.render('index')
+})
+
+.post('/viewOrder', function(req, res){
+  console.log(req.body);
+  db.collection('bestelling').insertOne({
+    firstname: req.body.fname,
+    email: req.body.email,
+    gender: req.body.gender,
+    size: req.body.size,
+    color: req.body.color,
+    text: req.body.text,
+    font: req.body.font,
+    fontColor: req.body.fontColor,
+    pos: req.body.pos,
+    land: req.body.land,
+    city: req.body.city,
+    postal: req.body.postal,
+    street: req.body.street,
+    housenumber: req.body.housenumber
+  }, viewOrder)
+
+  function viewOrder(err, data){
+    if(err){
+      console.log(err)
+    }else{
+      res.send('/viewOrder')
+    }
+  }
+})
+
+.listen(PORT, ()=> console.log(`App listening at http://localhost:${PORT}`));
